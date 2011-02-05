b2f61b946c0a0cebe166a02e7a41228bfd01259e
Pete Hodgson <git@thepete.net>
2011-02-05 10:59:51 -0800

    Add feature and scenario output (in different voices!)

--- lib/cuke_puke/formatter.rb
+++ lib/cuke_puke/formatter.rb
@@ -1,11 +1,31 @@
 module CukePuke
   class Formatter
+
+    VOICE_FOR = { 
+      :feature => 'vicki', 
+      :scenario => 'ralph',
+      :step => 'fred'
+    }
+
     def initialize( *args )
     end
 
     def before_step( step )
-      desc = "#{step.keyword} #{step.name}"
-      system( 'say', desc ) 
+      say "#{step.keyword} #{step.name}", VOICE_FOR[:step]
+    end
+
+    def scenario_name(keyword, name, file_colon_line, source_indent)
+     say "scenario: #{name}", VOICE_FOR[:scenario] 
+    end
+
+    def before_feature(feature)
+      say "feature: #{feature.name}", VOICE_FOR[:feature] 
+    end
+
+    private
+    
+    def say( what_to_say, voice = 'alex' )
+     system 'say', '-v', voice, what_to_say  
     end
   end
 end
